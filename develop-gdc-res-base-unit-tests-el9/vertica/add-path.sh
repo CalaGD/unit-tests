3d89d6d2cd65f1d3eb06188e8090c8ae79da75e2
calaGD <antonin.cala@gooddata.com>
2023-10-30 08:13:33 +0100

    cleaned up gdc-res-base

--- develop-gdc-res-base-unit-tests-el9/vertica/add-path.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-VERTICA_NODE=$( /opt/vertica/bin/vsql -U vertica -A -t -c "select node_name from nodes" )
-SQL="SELECT ADD_LOCATION( '/tmp/mddwh_vertica_export_temp', '$VERTICA_NODE', 'USER' );"
-vsql -U vertica -c "$SQL"
